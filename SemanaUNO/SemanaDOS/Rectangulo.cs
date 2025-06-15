8998b99b8baa4025daeb7f02e063c5a0a4a0945b
SonIsai09 <102713688+SonIsai09@users.noreply.github.com>
2025-06-14 23:10:00 -0600

    ejerucio 4

---
+++ SemanaUNO/SemanaDOS/Rectangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SemanaDOS
+{
+    internal class Rectangulo
+    {
+        public double Base { get; set; }
+        public double Altura { get; set; }
+
+        public double CalcularArea()
+        {
+            return Base * Altura; 
+        }
+
+        public double CalcularPerimetro()
+        {
+            return 2 * Base + 2 * Altura; 
+
+        }
+    }
+}
